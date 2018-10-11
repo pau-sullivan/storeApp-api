5e19b9d15aa4910850ec44bf8b52cb6b2ded2df1
Paula Sullivan <pau.sullivan@gmail.com>
2018-10-11 09:00:15 -0300

    api ok

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 
 // const PORT = process.env.PORT || 3301;
 //const PORT =3000;
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 3000;
 //app.use(express.static(__dirname + './dist/storAapp'));
 //app.use(express.static(path.join(__dirname, 'public')));
 app.use('/node_modules',  express.static(__dirname + '/node_modules')); // Use NodeModules
